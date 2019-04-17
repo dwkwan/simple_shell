adfe7cbf39c5551ab2cdf88b4f75ee93fb24130d
dwkwan <dwkwan1989@gmail.com>
2019-04-17 19:03:46 +0000

    First commit

---
+++ shell.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * main - runs the shell program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char *fullpathbuffer = NULL, *copy = NULL, *buffer = NULL;
+	char *PATH = NULL;
+	char **av;
+
+	PATH = _getenv("PATH");
+	if (PATH == NULL)
+		return (-1);
+	while (1)
+	{
+		av = NULL;
+		prompt();
+		buffer = _read();
+		if (*buffer != '\0')
+		{
+			av = tokenize(buffer);
+			if (av == NULL)
+			{
+				free(buffer);
+				continue;
+			}
+			fullpathbuffer = _fullpathbuffer(av, PATH, copy);
+			if (checkbuiltins(av, buffer) == 1)
+				continue;
+			_forkprocess(av, buffer, fullpathbuffer);
+		}
+		else
+			free(buffer);
+	}
+}
