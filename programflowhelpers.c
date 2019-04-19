80715b0b28207297fcb2544f601151431d350d42
dwkwan <dwkwan1989@gmail.com>
2019-04-19 20:45:37 +0000

    Add loop to handle comments

--- programflowhelpers.c
+++ programflowhelpers.c
@@ -27,6 +27,7 @@ char *_read(void)
 	ssize_t readcount = 0;
 	size_t n = 0;
 	char *buffer = NULL;
+	int i = 0;
 
 	readcount = getline(&buffer, &n, stdin);
 	if (readcount == -1)
@@ -38,6 +39,14 @@ char *_read(void)
 	}
 	if (buffer[readcount - 1] == '\n' || buffer[readcount - 1] == '\t')
 		buffer[readcount - 1] = '\0';
+	for (i = 0; buffer[i]; i++)
+	{
+		if (buffer[i] == '#')
+		{
+			buffer[i] = '\0';
+			break;
+		}
+	}
 	return (buffer);
 }
 /**
